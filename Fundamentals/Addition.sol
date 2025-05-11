a77aa9c3cc35430f921aa2d9da5206e06ffc6cfd
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-05-11 15:41:30 +0100

    Additional.col

---
+++ Fundamentals/Addition.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.9;
+
+contract SolidityFundamentals1 {
+    address public owner;
+    uint256 public x = 5;
+
+    constructor() {
+        owner = msg.sender
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    function add(uint256 y) public onlyOwner returns(uint256) {
+        require(y > x, "y should be greater than x");
+        uint256 z = x + y;
+        return z;
+    }
+}
